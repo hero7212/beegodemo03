4793d5eaf617f3eac9b661e4b0a97fc1c9d2216e
frontendgod <3054750147@qq.com>
2020-05-19 22:43:00 +0800

    p54 19:30

--- controllers/default.go
+++ controllers/default.go
@@ -7,9 +7,38 @@ import (
 type MainController struct {
 	beego.Controller
 }
+type Article struct {
+	Title   string
+	Content string
+}
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+
+	// 1、模板中绑定基本类型
+	c.Data["website"] = "beego 朱莉"
+	c.Data["title"] = "你好beego"
+	c.Data["num"] = 12
+	c.Data["falg"] = true
+
+	// 2、模板中绑定结构体数据
+	acticle := Article{
+		Title:   "我是golang教程",
+		Content: "beego小米商城",
+	}
+	c.Data["article"] = acticle
+
+	// 3、 range
+	c.Data["sliceList"] = []string{"php", "java", "golang"}
+
+	// 4、map
+	userinfo := make(map[string]interface{})
+	userinfo["username"] = "张三"
+	userinfo["age"] = 20
+	userinfo["sex"] = "男"
+	c.Data["userinfo"] = userinfo
+
+	// 4、遍历结构体类型的切片
+
+	c.TplName = "index.html"
+
 }
